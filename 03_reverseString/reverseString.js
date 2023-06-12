b8e1cf0e4a7b2f859ad6d54326dad35a9946cfb0
owen-makes <owen.botto@gmail.com>
2023-06-12 12:09:58 -0300

    Complete excercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let normal = text.split("")
+    let backwards = normal.reverse()
+    let commaSeparated = backwards.toString()
+    return commaSeparated.replaceAll(",","")
 };
 
 // Do not edit below this line
