8e4084dbb7831b69023c570f8f585756510996c4
owen-makes <owen.botto@gmail.com>
2023-07-05 08:20:49 -0300

    Complete excercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...args) {
+    const a = array
+    args.forEach((arg) =>{
+    if (a.indexOf(arg) > -1){
+        a.splice(a.indexOf(arg), 1)[0]
+             
+    }}) 
+    return a
 };
 
 // Do not edit below this line
