664d2408ce490e64efce0638c0ea034233075d24
owen-makes <owen.botto@gmail.com>
2023-07-14 11:18:58 -0300

    Complete exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let celsius = (farenheit - 32) * 5/9
+  return +celsius.toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  // (x × 9 / 5 + 32)
+  let farenheit = celsius * 9 / 5 + 32
+  return +farenheit.toFixed(1)
 };
 
 // Do not edit below this line
