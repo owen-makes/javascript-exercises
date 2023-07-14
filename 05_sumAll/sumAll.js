3e042cd799a3f51e131f84d7d4e9839866ee4a5e
owen-makes <owen.botto@gmail.com>
2023-07-14 10:47:30 -0300

    Complete exercise 05_sumAll and 06_leapYears

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(beg,end) {
 
+    if (typeof(beg) != "number" || typeof(end) != "number" || beg < 0 || end < 0 ){
+        return "ERROR"
+    } else {
+        const max = Math.max(beg, end)
+        const min = Math.min(beg,end)
+        let result = 0
+
+        for (let i = min; i <= max; i++){
+            result += i
+        }
+        return result 
+    }
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (typeof(year)!= "number" || year < 0){
+        return "ERROR"
+    }else if((year % 4 == 0 && year % 100 !== 0) || (year % 400 == 0 && year % 100 == 0)){
+        return true
+    } else {return false}
 };
 
 // Do not edit below this line
