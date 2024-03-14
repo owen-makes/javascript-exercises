2014a751da7f412fcc54b19d2377cd189fb9ab3b
owen-makes <owen.botto@gmail.com>
2024-03-14 10:52:49 -0300

    Complete excercise 11 and 12

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    function isTitle(object){
+        return object.title
+    }
+    
+    return array.map(isTitle)
+  
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+    const oldest = arr.sort(function(a,b){
+        let lastItem
+        let nextItem
+        const d = new Date().getFullYear()
+        if ('yearOfDeath' in a){
+            lastItem = a.yearOfDeath - a.yearOfBirth
+        } else {
+            lastItem = d - a.yearOfBirth
+        }
+        if ('yearOfDeath' in b){
+            nextItem = b.yearOfDeath - b.yearOfBirth
+        } else {
+            nextItem = d - b.yearOfBirth
+        }
+        return lastItem > nextItem ? -1 : 1
+        
+    })
+    
+    return oldest[0]
+    
 };
 
 // Do not edit below this line
